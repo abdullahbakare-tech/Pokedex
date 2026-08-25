73679b03371750d283a6823bf6017bdbe38a0b9d
abdullahbakare-tech <abdullahbakare@gmail.com>
2026-08-24 20:08:40 -0400

    Created the App.js, Pokecard.js, & Pokedex.js

--- Pokecard.js
+++ Pokecard.js
@@ -1,4 +1,12 @@
 /** This will be used to show a single pokemon card with their name, image, and type*/
-function Pokecard(props) {
-  return <div>{props.children}</div>;
+function Pokecard({ id, name, type, base_experience }) {
+  const image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+  return (
+    <div className="Pokecard">
+      <h3>{name}</h3>
+      {image}
+      <p>Type: {type}</p>
+      <p>Experience: {base_experience}</p>
+    </div>
+  );
 }
--- Pokedex.js
+++ Pokedex.js
@@ -1,4 +1,33 @@
 /**This shall be used to describe each pokemon, then renders a sequence of Pokecard components */
-function Pokedex({ id, name, type, base_experience }) {
-  return <div></div>;
+function Pokedex({ pokemon }) {
+  Pokedex.defaultProps = {
+    pokemon: [
+      { id: 4, name: "Charmander", type: "fire", base_experience: 62 },
+      { id: 7, name: "Squirtle", type: "water", base_experience: 63 },
+      { id: 11, name: "Metapod", type: "bug", base_experience: 72 },
+      { id: 12, name: "Butterfree", type: "flying", base_experience: 178 },
+      { id: 25, name: "Pikachu", type: "electric", base_experience: 112 },
+      { id: 39, name: "Jigglypuff", type: "normal", base_experience: 95 },
+      { id: 94, name: "Gengar", type: "poison", base_experience: 225 },
+      { id: 133, name: "Eevee", type: "normal", base_experience: 65 },
+    ],
+  };
+
+  return (
+    <div className="Pokedex">
+      <h1>Pokedex</h1>
+
+      <div className="Pokedex-cards">
+        {pokemon.map((p) => (
+          <Pokecard
+            key={p.id}
+            id={p.id}
+            name={p.name}
+            type={p.type}
+            exp={p.base_experience}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
